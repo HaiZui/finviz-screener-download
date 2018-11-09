0cb6c6770992c3408c848da13831f31583758fc2
Teemu Parviainen <parviainen.teemu@gmail.com>
2018-11-09 18:59:19 +0200

    Updated dependencies

--- lib/setup.py
+++ lib/setup.py
@@ -25,7 +25,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandas', 'numpy', 'datetime', 'mysql', 'bs4','apache-airflow'],
+    install_requires=['pandas', 'numpy', 'datetime', 'mysqlclient', 'bs4','apache-airflow'],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
