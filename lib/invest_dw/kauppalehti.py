e16e8dca9294a331d9bd7a7b2780e052deb6d55a
parviainen.teemu@gmail.com <parviainen.teemu@gmail.com>
2019-01-02 20:54:35 +0200

    Updated Kauppalehti stock data extraction functions due to new version of webpage

--- lib/invest_dw/kauppalehti.py
+++ lib/invest_dw/kauppalehti.py
@@ -5,48 +5,96 @@ import time
 import logging
 import invest_dw.scraping as scraping
 from bs4 import BeautifulSoup
+import ast
 
+def find_parens(s):
+    # Helping function for finding the closing parenthesis
+    toret = {}
+    pstack = []
 
+    for i, c in enumerate(s):
+        if c == '[':
+            pstack.append(i)
+        elif c == ']':
+            if len(pstack) == 0:
+                raise IndexError("No matching closing parens at: " + str(i))
+            toret[pstack.pop()] = i
 
-def stock_data():
-    columns = ['Nimi',
-                'Viim',
-                'Muutos',
-                'Aika',
-                'Osto',
-                'Myynti',
-                'Ylin',
-                'Alin',
-                'MiljE',
-                'Porssi',
-                'Teollisuus',
-                'Timestamp']
+    if len(pstack) > 0:
+        #raise IndexError("No matching opening parens at: " + str(pstack.pop()))
+        # We want only closing parenthesis (of the first encountered list)
+        pass
+
+    return toret
+
+def stock_data(exchange):
+    columns = ['closePrice'
+               , 'marketValue'
+               , 'tradeCurrency'
+               , 'tickSize'
+               , 'company'
+               , 'closeDateTime'
+               , 'insRef'
+               , 'internalTurnover'
+               , 'isin'
+               , 'internalQuantity'
+               , 'name'
+               , 'quantity'
+               , 'turnover'
+               , 'dayLowPrice'
+               , 'lastPrice'
+               , 'openPrice'
+               , 'dayHighPrice'
+               , 'changePercent1m'
+               , 'askPrice'
+               , 'changePercent'
+               , 'bidPrice'
+               , 'symbol'
+               #, 'numberOfShares'
+               #, 'totalCompanyShares'
+               ]
+    all_columns = columns.copy()
+    all_columns.append('numberOfShares')
+    all_columns.append('totalCompanyShares')
+    all_columns.append('Timestamp')
 
     #url = 'https://www.kauppalehti.fi/5/i/porssi/porssikurssit/lista.jsp'
-    url = 'https://www.kauppalehti.fi/5/i/porssi/porssikurssit/lista.jsp?reverse=false&order=alpha&markets=XHEL&markets=XSTO&markets=XCSE&volume=cur&psize=50&listIds=kaikki&rdc=166f95744d8&gics=0&refresh=60&currency=euro'
+    url = 'https://www.kauppalehti.fi/porssi/kurssit/{}'.format(exchange.upper())
+
 
     r = scraping.request_random_header(url)
     the_page = r.text
     soup = BeautifulSoup(the_page,'lxml')
     
-    
-    table = soup.find_all('div',{'id':'taulukonTiedot'})
-    rows = table[0].find_all('tr')
-    
-    data = []    
-    group = ""
-    for row in rows:
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        if len(cols) == 1:
-            group = row.find_all('td')[0].text
-            continue
-        
-        if row['class'][0] != "strong":
-            cols.append(group)
-            timestamp = time.time()
-            cols.append(timestamp)
-            data.append([ele for ele in cols if ele]) # Get rid of empty values
+    # Find list containing share prices etc. from the string
+    i = soup.text.find('shares')
+    # Adjust
+    i += 8
 
-    return columns,data
+    list_str = soup.text[i:]
+
+    # List of  interest starts at position i
+    # Find location of enclosing parenthesis
+    list_str = list_str[:find_parens(list_str)[0]+1]
+   
+    list_obj = ast.literal_eval(list_str)
+    
+    # Construct returned data
+    data = []    
+
+    for stock in list_obj:
+        timestamp = time.time()
+        row = []
+        for column in columns:
+            try:
+                row.append(stock[column])
+            except:
+                row.append(None)		
+        row.append(stock['quoteShares'][0]['numberOfShares'])
+        row.append(stock['quoteShares'][0]['totalCompanyShares'])
+        row.append(timestamp)
+        data.append(row)
+
+
+    return all_columns,data
 	
